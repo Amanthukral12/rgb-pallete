6bbcdd3656d88e511cb24087ce38ce8d50d8f763
Aman Thukral <thukral618@gmail.com>
2023-07-24 22:10:59 +0530

    Adding copy to clipboard feature

--- src/components/ColorPaletteItem.jsx
+++ src/components/ColorPaletteItem.jsx
@@ -1,12 +1,27 @@
 import propTypes from "prop-types";
 import "../styles/ColorPaletteItem.css";
 import { BiCopy } from "react-icons/bi";
+import { useState } from "react";
 const ColorPaletteItem = ({ rgb, hex }) => {
+  const [copied, setCopied] = useState(false);
+  const copyColor = (e) => {
+    const color = e.target.innerText;
+    navigator.clipboard.writeText(color);
+  };
   return (
     <>
       <div className="colorItem" style={{ background: rgb }}>
-        <div className="hexContainer">
-          <span>{hex}</span>
+        <div
+          className="hexContainer"
+          onClick={(e) => {
+            copyColor(e);
+            setCopied(true);
+            setTimeout(() => {
+              setCopied(false);
+            }, 1000);
+          }}
+        >
+          <span>{copied ? "Copied!" : hex}</span>
           <BiCopy className="icon" />
         </div>
       </div>
